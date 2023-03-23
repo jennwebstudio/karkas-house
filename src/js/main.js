a35ec9964df3238ecf1fa245a48bb117e7e6489d
Eugenia Ivanova <evgenia.ivanova_ru@yahoo.com>
2023-03-23 19:05:17 +0400

    finish header including adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,16 @@ $(function () {
     
   $('.is-drop').on('click', function () {
     $('.dropdown').toggleClass('dropdown--active');
-    $('.list-icon').toggleClass('list-icon--active');
+    $('.arrow-icon').toggleClass('arrow-icon--active');
   });
 
   $(document).on('click', function (e) { // событие клика по веб-документу
 		const menu = $('.menu'),
           drop = $('.dropdown');
-		if ( !menu.is(e.target) & menu.has(e.target).length === 0 ) { //  если клик был не по menu, не по его дочерним элементам
+		if ( !menu.is(e.target) & menu.has(e.target).length === 0 ) { 
+      //  если клик был не по menu, не по его дочерним элементам
 			drop.removeClass('dropdown--active');
+      $('.arrow-icon').removeClass('arrow-icon--active');
 		}
 	});
   
